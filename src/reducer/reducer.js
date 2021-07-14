b5e2ce45b032c167f250ed9c2dcde5b6cc182e79
jithesh-dev <jithesh.kumar.j@gmail.com>
2021-07-14 23:30:46 +0100

    fix: removed comments and updated package file

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,20 +1,6 @@
 import db from "../firebase";
 export const initialState = {
   basket: [
-    // {
-    //   id: 1080543,
-    //   name: "STAR WARS 40TH BOX",
-    //   catagory: "Star Wars infiknit",
-    //   gender: "Men",
-    //   price: 85,
-    //   qty: 4,
-    //   image_1:
-    //     "https://www.stance.com/dw/image/v2/BBBN_PRD/on/demandware.static/-/Sites-masterCatalog_Stance/default/dw6307a569/prod_images/A545B20STA_MUL.jpg?sw=388&sh=480&sm=fit&q=85",
-    //   image_2:
-    //     "https://www.stance.com/dw/image/v2/BBBN_PRD/on/demandware.static/-/Sites-masterCatalog_Stance/default/dw7b5a2527/prod_images/A545B20STA_MUL_ALT_02.jpg?sw=388&sh=480&sm=fit&q=85",
-    //   image_3:
-    //     "https://www.stance.com/dw/image/v2/BBBN_PRD/on/demandware.static/-/Sites-masterCatalog_Stance/default/dw11a2128f/prod_images/A558B20WIL_OFW_ALT_02.jpg?sw=388&sh=480&sm=fit&q=85",
-    // },
   ],
   user: null,
 };
@@ -111,34 +97,3 @@ function reducer(state, action) {
 }
 
 export default reducer;
-
-// async function addDB(uid, { id, name, catagory, price, image_1, gender, qty }) {
-//   console.log("object");
-//   // try {
-//   //   await db
-//   //     .collection("users")
-//   //     .doc(uid)
-//   //     .collection("cartItems")
-//   //     .add({ id, name, catagory, price, image_1, gender, qty: 1 });
-//   // } catch (err) {
-//   //   console.log("fetch failed", err);
-//   // }
-// }
-
-// async function getDB(uid) {
-//   let item = [];
-//   try {
-//     // item = await db.collection("users").doc(uid).collection("cartItems").get();
-//     const snap = await db
-//       .collection("users")
-//       .doc(uid)
-//       .collection("cartItems")
-//       .onSnapshot((snapshot) =>
-//         snapshot.docs.map((doc) => item.push(doc.data()))
-//       );
-//     return "snap";
-//   } catch (err) {
-//     console.log("fetch failed", err);
-//     return null;
-//   }
-// }
